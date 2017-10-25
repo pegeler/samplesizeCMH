cd4f9dfc8a19aef40857eb2e110e7ab2226ff6b1
pegeler <paulegeler@gmail.com>
2017-10-24 21:58:20 -0400

    Added examples to odds.ratio.r and improved other documentation

--- R/odds.ratio.r
+++ R/odds.ratio.r
@@ -1,8 +1,26 @@
-#' Create odds ratios estimates from a table
+#' Create an odds ratio estimate from a 2-by-2 table of frequencies or proportions
 #'
-#' @param x A two-dimensional matrix or table containing frequencies
+#' @param x A two-dimensional matrix or table containing frequencies or proportions
 #'
 #' @return A numeric vector.
+#' @examples
+#' # Load in Titanic data from datasets package
+#' data(Titanic, package = "datasets")
+#'
+#' # Get marginal table of survival by sex
+#' marginal_table <- margin.table(Titanic, c(2,4))
+#' marginal_table
+#'
+#' # Compute odds ratio of marginal table
+#' odds.ratio(marginal_table)
+#'
+#' # Get partial tables of survival by sex, stratified by class
+#' partial_tables <- margin.table(Titanic, c(2,4,1))
+#' partial_tables
+#'
+#' # Compute odds ratio of each partial table
+#' apply(partial_tables, 3, odds.ratio)
+#'
 #' @author Paul W. Egeler, M.S.
 #' @export
 odds.ratio <- function (x) {
