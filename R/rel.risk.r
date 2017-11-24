e2c3a9482e469c8ad51913e719374a4c2c880b88
pegeler <paulegeler@gmail.com>
2017-11-24 13:10:34 -0500

    Adding relative risk function.

---
+++ R/rel.risk.r
@@ -0,0 +1,15 @@
+#' Calculate the relative risk from a 2-by-2 table
+#'
+#' Computes the relative risk of a specified column of a two-by-two table.
+#'
+#' @param x A table or matrix containing frequencies
+#' @param col.num The column number upon which relative risk should be calculated
+#'
+#' @return A numeric vector.
+#' @author Paul W. Egeler, M.S.
+#' @export
+rel.risk <- function(x, col.num = 1) {
+  stopifnot(identical(dim(x), c(2L,2L)))
+  y <- prop.table(x,1)[,col.num]
+  y[1] / y[2]
+}
