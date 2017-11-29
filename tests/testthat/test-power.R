37a31d4eef0d4bd04d57148eaed70a32d6445da6
pegeler <paulegeler@gmail.com>
2017-11-28 20:46:45 -0500

    Added tests to check that sign change logic works

--- tests/testthat/test-power.R
+++ tests/testthat/test-power.R
@@ -62,3 +62,64 @@ test_that("Return same values as Wittes and Wallenstein Table 3", {
   expect_equal(power_test(c(3,3),c(0.2,0.5),80,TRUE),0.524)
   expect_equal(power_test(c(4,4),c(0.2,0.5),80,TRUE),0.742)
 })
+
+
+test_that("Corrected power is less than uncorrected", {
+  correction_test <- function(thetas, p2, N, alternative) {
+    vpower <- Vectorize(power.cmh.test,"correct", SIMPLIFY = FALSE)
+
+    power_pair <-
+      sapply(
+        vpower(
+          theta = thetas,p2 = p2,N = N,power = NULL,
+          alternative = alternative,correct = c(FALSE,TRUE)
+        ),
+        "[[",
+        "power"
+    )
+
+    power_pair[1] > power_pair[2]
+  }
+
+  # Two-sided
+  expect_true(correction_test(c(12,12),c(0.2,0.25),16,"two"))
+  expect_true(correction_test(c(10,10),c(0.2,0.25),28,"two"))
+  expect_true(correction_test(c(8,8),c(0.2,0.25),40,"two"))
+  expect_true(correction_test(c(8,8),c(0.2,0.5),40,"two"))
+  expect_true(correction_test(c(4,8),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(c(2,6),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(c(3,3),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(c(3,3),c(0.2,0.5),80,"two"))
+  expect_true(correction_test(c(4,4),c(0.2,0.5),80,"two"))
+  expect_true(correction_test(1 / c(12,12),c(0.2,0.25),16,"two"))
+  expect_true(correction_test(1 / c(10,10),c(0.2,0.25),28,"two"))
+  expect_true(correction_test(1 / c(8,8),c(0.2,0.25),40,"two"))
+  expect_true(correction_test(1 / c(8,8),c(0.2,0.5),40,"two"))
+  expect_true(correction_test(1 / c(4,8),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(1 / c(2,6),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(1 / c(3,3),c(0.2,0.5),60,"two"))
+  expect_true(correction_test(1 / c(3,3),c(0.2,0.5),80,"two"))
+  expect_true(correction_test(1 / c(4,4),c(0.2,0.5),80,"two"))
+
+  # Greater
+  expect_true(correction_test(c(12,12),c(0.2,0.25),16,"g"))
+  expect_true(correction_test(c(10,10),c(0.2,0.25),28,"g"))
+  expect_true(correction_test(c(8,8),c(0.2,0.25),40,"g"))
+  expect_true(correction_test(c(8,8),c(0.2,0.5),40,"g"))
+  expect_true(correction_test(c(4,8),c(0.2,0.5),60,"g"))
+  expect_true(correction_test(c(2,6),c(0.2,0.5),60,"g"))
+  expect_true(correction_test(c(3,3),c(0.2,0.5),60,"g"))
+  expect_true(correction_test(c(3,3),c(0.2,0.5),80,"g"))
+  expect_true(correction_test(c(4,4),c(0.2,0.5),80,"g"))
+
+  # Less
+  expect_true(correction_test(1 / c(12,12),c(0.2,0.25),16,"l"))
+  expect_true(correction_test(1 / c(10,10),c(0.2,0.25),28,"l"))
+  expect_true(correction_test(1 / c(8,8),c(0.2,0.25),40,"l"))
+  expect_true(correction_test(1 / c(8,8),c(0.2,0.5),40,"l"))
+  expect_true(correction_test(1 / c(4,8),c(0.2,0.5),60,"l"))
+  expect_true(correction_test(1 / c(2,6),c(0.2,0.5),60,"l"))
+  expect_true(correction_test(1 / c(3,3),c(0.2,0.5),60,"l"))
+  expect_true(correction_test(1 / c(3,3),c(0.2,0.5),80,"l"))
+  expect_true(correction_test(1 / c(4,4),c(0.2,0.5),80,"l"))
+})
